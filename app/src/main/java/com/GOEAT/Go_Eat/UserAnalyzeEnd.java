556e9ce88f03ef0d108bc920d921f7bd3d2b5dbb
kimjungchun <wjdcjs22@naver.com>
2020-07-30 22:29:05 +0900

    메인화면에 위치 정보 추가

--- app/src/main/java/com/GOEAT/Go_Eat/UserAnalyzeEnd.java
+++ app/src/main/java/com/GOEAT/Go_Eat/UserAnalyzeEnd.java
@@ -2,8 +2,11 @@ package com.GOEAT.Go_Eat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.android.volley.RequestQueue;
@@ -13,12 +16,14 @@ public class UserAnalyzeEnd extends AppCompatActivity {
 
     private ImageView img_char;
     public RequestQueue queue;
+    private Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_analyze_end);
 
+        btn_next = findViewById(R.id.btn_next);
         img_char = findViewById(R.id.img_char);
 
         // 사용자 캐릭터 설정
@@ -26,5 +31,13 @@ public class UserAnalyzeEnd extends AppCompatActivity {
         String email = prefs.getString("email","");
         UserDB userDB = new UserDB();
         userDB.setImageToUserChar(img_char, email,UserAnalyzeEnd.this);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity_after.class);
+                startActivity(intent);
+            }
+        });
     }
 }
