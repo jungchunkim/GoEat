ac53e3f185417e5cd9fe1bb1966a11fce8ff6467
kimjungchun <wjdcjs22@naver.com>
2020-09-20 22:48:09 +0900

    위치 선택 페이지 최종

--- app/src/main/java/com/GOEAT/Go_Eat/location_check.java
+++ app/src/main/java/com/GOEAT/Go_Eat/location_check.java
@@ -3,7 +3,11 @@ package com.GOEAT.Go_Eat;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -14,10 +18,28 @@ public class location_check extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_check);
 
+        Button btn_next=(Button)findViewById(R.id.btn_next);
+        ImageView btn_back= (ImageView)findViewById(R.id.btn_back);
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("서울 강북"));
         tabs.addTab(tabs.newTab().setText("서울 강남"));
         tabs.setTabGravity(tabs.GRAVITY_FILL);
+
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), investigation_page.class);
+                startActivity(intent);
+            }
+        });
 
         //Adapter
         final ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
