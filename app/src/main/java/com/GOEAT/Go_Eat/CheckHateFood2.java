c6af61e4c4268d7d353e0f4066de0f540a25ef03
MinYoung-Lim <minyounge17@naver.com>
2020-09-20 23:11:03 +0900

    CheckHateFood2 스크롤뷰 적용 by.민영

--- app/src/main/java/com/GOEAT/Go_Eat/CheckHateFood2.java
+++ app/src/main/java/com/GOEAT/Go_Eat/CheckHateFood2.java
@@ -21,15 +21,15 @@ import org.json.JSONObject;
 
 public class CheckHateFood2 extends AppCompatActivity implements View.OnClickListener{
 
-    Button btn_1, btn_2, btn_3, btn_4,btn_5, btn_6,btn_7,btn_8,btn_9,btn_10,btn_11,btn_12, btn_next;
-    private int[] clickCheck = new int[12];
+    Button btn_1, btn_2, btn_3, btn_4,btn_5, btn_6,btn_7,btn_8,btn_9,btn_10,btn_11,btn_12,btn_13,btn_14,btn_15,btn_16,btn_17,btn_18,btn_19,btn_20,btn_21,btn_22,btn_23,btn_24, btn_next;
+    private int[] clickCheck = new int[24];
     TextView tv_txtWithName;
     ImageView iv_back;
 
     //2020-08-29 염상희
     //싫어하는 음식 저장 부분 추가
     final private UserDB userDB = new UserDB();
-    private String[] foodlist = new String[12];
+    private String[] foodlist = new String[24];
     ImageView img_char;
     private String Hatefoodlists = "N";
 
@@ -53,6 +53,19 @@ public class CheckHateFood2 extends AppCompatActivity implements View.OnClickLis
         btn_10 = findViewById(R.id.btn_10);
         btn_11 = findViewById(R.id.btn_11);
         btn_12 = findViewById(R.id.btn_12);
+        btn_13 = findViewById(R.id.btn_13);
+        btn_14 = findViewById(R.id.btn_14);
+        btn_15 = findViewById(R.id.btn_15);
+        btn_16 = findViewById(R.id.btn_16);
+        btn_17 = findViewById(R.id.btn_17);
+        btn_18 = findViewById(R.id.btn_18);
+        btn_19 = findViewById(R.id.btn_19);
+        btn_20 = findViewById(R.id.btn_20);
+        btn_21 = findViewById(R.id.btn_21);
+        btn_22 = findViewById(R.id.btn_22);
+        btn_23 = findViewById(R.id.btn_23);
+        btn_24 = findViewById(R.id.btn_24);
+
         btn_next = findViewById(R.id.btn_next);
         tv_txtWithName = findViewById(R.id.tv_txtWithName);
         iv_back = findViewById(R.id.iv_back);
@@ -76,6 +89,18 @@ public class CheckHateFood2 extends AppCompatActivity implements View.OnClickLis
         btn_10.setOnClickListener(this);
         btn_11.setOnClickListener(this);
         btn_12.setOnClickListener(this);
+        btn_13.setOnClickListener(this);
+        btn_14.setOnClickListener(this);
+        btn_15.setOnClickListener(this);
+        btn_16.setOnClickListener(this);
+        btn_17.setOnClickListener(this);
+        btn_18.setOnClickListener(this);
+        btn_19.setOnClickListener(this);
+        btn_20.setOnClickListener(this);
+        btn_21.setOnClickListener(this);
+        btn_22.setOnClickListener(this);
+        btn_23.setOnClickListener(this);
+        btn_24.setOnClickListener(this);
 
         //랜덤으로 받아온 음식 index들로 이미지 설정하는 부분
         Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -100,6 +125,19 @@ public class CheckHateFood2 extends AppCompatActivity implements View.OnClickLis
                         btn_10.setText(foodlist[9]);
                         btn_11.setText(foodlist[10]);
                         btn_12.setText(foodlist[11]);
+                        btn_13.setText(foodlist[12]);
+                        btn_14.setText(foodlist[13]);
+                        btn_15.setText(foodlist[14]);
+                        btn_16.setText(foodlist[15]);
+                        btn_17.setText(foodlist[16]);
+                        btn_18.setText(foodlist[17]);
+                        btn_19.setText(foodlist[18]);
+                        btn_20.setText(foodlist[19]);
+                        btn_21.setText(foodlist[20]);
+                        btn_22.setText(foodlist[21]);
+                        btn_23.setText(foodlist[22]);
+                        btn_24.setText(foodlist[23]);
+
 
                     }else {
 
@@ -143,7 +181,7 @@ public class CheckHateFood2 extends AppCompatActivity implements View.OnClickLis
                     };
 
 
-                    for (int i = 0; i < 12; i++) {
+                    for (int i = 0; i < 24; i++) {
                         if (Hatefoodlists.equals("N") && clickCheck[i] == -1) {
                             Hatefoodlists = "\"" + foodlist[i] + "\"";
                         } else if (clickCheck[i] == -1) {
@@ -287,6 +325,114 @@ public class CheckHateFood2 extends AppCompatActivity implements View.OnClickLis
                 } else {
                     reChangeBtnBackground(btn_12);
                     clickCheck[11] = 1;
+                }
+                break;
+            case R.id.btn_13:
+                if (clickCheck[12] == 1) {
+                    changeBtnBackground(btn_13);
+                    clickCheck[12] = -1;
+                } else {
+                    reChangeBtnBackground(btn_13);
+                    clickCheck[12] = 1;
+                }
+                break;
+            case R.id.btn_14:
+                if (clickCheck[13] == 1) {
+                    changeBtnBackground(btn_14);
+                    clickCheck[13] = -1;
+                } else {
+                    reChangeBtnBackground(btn_12);
+                    clickCheck[13] = 1;
+                }
+                break;
+            case R.id.btn_15:
+                if (clickCheck[14] == 1) {
+                    changeBtnBackground(btn_15);
+                    clickCheck[14] = -1;
+                } else {
+                    reChangeBtnBackground(btn_15);
+                    clickCheck[14] = 1;
+                }
+                break;
+            case R.id.btn_16:
+                if (clickCheck[15] == 1) {
+                    changeBtnBackground(btn_16);
+                    clickCheck[15] = -1;
+                } else {
+                    reChangeBtnBackground(btn_16);
+                    clickCheck[15] = 1;
+                }
+                break;
+            case R.id.btn_17:
+                if (clickCheck[16] == 1) {
+                    changeBtnBackground(btn_17);
+                    clickCheck[16] = -1;
+                } else {
+                    reChangeBtnBackground(btn_17);
+                    clickCheck[16] = 1;
+                }
+                break;
+            case R.id.btn_18:
+                if (clickCheck[17] == 1) {
+                    changeBtnBackground(btn_18);
+                    clickCheck[17] = -1;
+                } else {
+                    reChangeBtnBackground(btn_18);
+                    clickCheck[17] = 1;
+                }
+                break;
+            case R.id.btn_19:
+                if (clickCheck[18] == 1) {
+                    changeBtnBackground(btn_19);
+                    clickCheck[18] = -1;
+                } else {
+                    reChangeBtnBackground(btn_19);
+                    clickCheck[18] = 1;
+                }
+                break;
+            case R.id.btn_20:
+                if (clickCheck[19] == 1) {
+                    changeBtnBackground(btn_20);
+                    clickCheck[19] = -1;
+                } else {
+                    reChangeBtnBackground(btn_20);
+                    clickCheck[19] = 1;
+                }
+                break;
+            case R.id.btn_21:
+                if (clickCheck[20] == 1) {
+                    changeBtnBackground(btn_21);
+                    clickCheck[20] = -1;
+                } else {
+                    reChangeBtnBackground(btn_21);
+                    clickCheck[20] = 1;
+                }
+                break;
+            case R.id.btn_22:
+                if (clickCheck[21] == 1) {
+                    changeBtnBackground(btn_22);
+                    clickCheck[21] = -1;
+                } else {
+                    reChangeBtnBackground(btn_22);
+                    clickCheck[21] = 1;
+                }
+                break;
+            case R.id.btn_23:
+                if (clickCheck[22] == 1) {
+                    changeBtnBackground(btn_23);
+                    clickCheck[22] = -1;
+                } else {
+                    reChangeBtnBackground(btn_23);
+                    clickCheck[22] = 1;
+                }
+                break;
+            case R.id.btn_24:
+                if (clickCheck[23] == 1) {
+                    changeBtnBackground(btn_24);
+                    clickCheck[23] = -1;
+                } else {
+                    reChangeBtnBackground(btn_24);
+                    clickCheck[23] = 1;
                 }
                 break;
         }
