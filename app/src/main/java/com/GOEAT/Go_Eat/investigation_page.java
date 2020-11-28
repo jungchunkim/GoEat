90c3d462f4eb6a858e590f1e1e18276ec16d62bd
yeomsh <sanghee2911@gmail.com>
2020-11-28 22:08:35 +0900

    조사홈 버튼 선택 값 확인 필요

--- app/src/main/java/com/GOEAT/Go_Eat/investigation_page.java
+++ app/src/main/java/com/GOEAT/Go_Eat/investigation_page.java
@@ -169,10 +169,14 @@ public class investigation_page extends AppCompatActivity implements View.OnClic
 //                editor.commit();
                 Intent intent = new Intent(getApplicationContext(), AnalysisHomeRealActivity.class);
                 //칼로리
-                if(clickCheck[0]==1)
-                    intent.putExtra("calo","high");
-                else if(clickCheck[1]==1)
-                    intent.putExtra("calo","low");
+                if(clickCheck[0]==1) {
+                    Log.e("******", "high");
+                    intent.putExtra("calo", "high");
+                }
+                else if(clickCheck[1]==1) {
+                    Log.e("******", "low");
+                    intent.putExtra("calo", "low");
+                }
                 else
                     intent.putExtra("calo","null");
 
@@ -185,31 +189,45 @@ public class investigation_page extends AppCompatActivity implements View.OnClic
                     intent.putExtra("loc","null");
 
                 //who
-                if(clickCheck_1[0]==1)
-                intent.putExtra("who","혼자");
-                else if(clickCheck_1[1]==1)
+                if(clickCheck_1[0]==1){
+                    Log.e("******","혼자");
+                    intent.putExtra("who","혼자");
+                }
+                else if(clickCheck_1[1]==1){
+                    Log.e("******","애인");
                     intent.putExtra("who","애인");
-                else if(clickCheck_1[2]==1)
-                    intent.putExtra("who","친구");
-                else if(clickCheck_1[3]==1)
-                    intent.putExtra("who","회식");
-                else if(clickCheck_1[4]==1)
-                    intent.putExtra("who","가족");
+                }
+                else if(clickCheck_1[2]==1) {
+                    intent.putExtra("who", "친구");
+                    Log.e("******","친구");
+                }
+                else if(clickCheck_1[3]==1){
+                    intent.putExtra("who", "회식");
+                    Log.e("******","회식");
+                }
+                else if(clickCheck_1[4]==1){
+                    intent.putExtra("who", "가족");
+                    Log.e("******","가족");
+                }
                 else
                     intent.putExtra("who","null");
 
                 //감정
-                if(clickCheck_3[0]==1)
-                    intent.putExtra("emo","설레는");
-                else if(clickCheck_3[1]==1)
+                if(clickCheck_3[0]==1) {
+                    Log.e("******", "설레는");
+                    intent.putExtra("emo", "설레는");
+                }
+                else if(clickCheck_3[1]==1){
+                    Log.e("******","축하하는");
                     intent.putExtra("emo","축하하는");
+                }
                 else if(clickCheck_3[2]==1)
                     intent.putExtra("emo","우울한");
                 else if(clickCheck_3[3]==1)
                     intent.putExtra("emo","평범한");
                 else if(clickCheck_3[4]==1)
                     intent.putExtra("emo","스트레스");
-                else if(clickCheck_3[4]==1)
+                else if(clickCheck_3[5]==1)
                     intent.putExtra("emo","행복한");
                 else
                     intent.putExtra("emo","null");
