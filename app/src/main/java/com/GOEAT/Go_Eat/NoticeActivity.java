d85ddeee6761064fbb2056f1cf893fb23b365469
kimjungchun <wjdcjs22@naver.com>
2020-12-01 16:48:13 +0900

    공지사항 완료, 회원가입 약관 동의 완료

--- app/src/main/java/com/GOEAT/Go_Eat/NoticeActivity.java
+++ app/src/main/java/com/GOEAT/Go_Eat/NoticeActivity.java
@@ -2,13 +2,39 @@ package com.GOEAT.Go_Eat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class NoticeActivity extends AppCompatActivity {
+
+    LinearLayout layout_notice1, layout_notice2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
+
+        layout_notice1=(LinearLayout)findViewById(R.id.layout_notice1);
+        layout_notice2=(LinearLayout)findViewById(R.id.layout_notice2);
+
+
+        layout_notice1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Notice_detail1.class);
+                startActivity(intent);
+            }
+        });
+
+        layout_notice2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Notice_detail2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/GOEAT/Go_Eat/Notice_detail2.java
+++ app/src/main/java/com/GOEAT/Go_Eat/Notice_detail2.java
@@ -4,27 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.ImageView;
 
-public class Service_Detail_2 extends AppCompatActivity {
+public class Notice_detail2 extends AppCompatActivity {
 
-    private WebView webview;
     ImageView iv_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_service__detail_2);
-
+        setContentView(R.layout.activity_notice_detail2);
 
         iv_back=findViewById(R.id.iv_back);
-        webview = (WebView) findViewById(R.id.webview2);
-
-
-
-
-        webview.loadUrl("file:///android_asset/service_detail2.html");
 
         iv_back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +23,5 @@ public class Service_Detail_2 extends AppCompatActivity {
                 onBackPressed();
             }
         });
-
     }
 }
